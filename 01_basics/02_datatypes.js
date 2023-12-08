b44bef97765c06baabef00e47c98688c836a25bf
Akash Gaur <122721474+Gaur2526@users.noreply.github.com>
2023-12-08 18:47:37 +0000

    hello

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+let name= "akash"
+let age = "20"
+let isMale = true
+let state;
+
+// data types : 
+// number
+// string
+// boolean
+// null => standlone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof "akash")
+console.log(typeof null)
+console.log(typeof undefined)
