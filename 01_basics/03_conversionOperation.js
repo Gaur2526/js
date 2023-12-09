36534ef038ab110003bca26284a8b9357135c79d
Akash Gaur <122721474+Gaur2526@users.noreply.github.com>
2023-12-09 20:24:04 +0000

    conversion of data types

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,6 +1,21 @@
-let score = "33"
+let score = "33abc"
 
 console.log(typeof score);
 
 let typeInNumber = Number(score)
 console.log(typeof typeInNumber);
+console.log(typeInNumber)
+
+// "33"=> 33
+// "33abc" =>nan
+// "true" =>1 , "false"=>0
+
+let isLoggedIn ;
+
+let booleanisLoggedIn = Boolean(isLoggedIn);
+console.log(booleanisLoggedIn)
+
+let number  = 33
+
+let stringnumber = String(number)
+console.log(typeof stringnumber)
