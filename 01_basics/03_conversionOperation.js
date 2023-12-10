5f56dc9e808f68bc568919a196300d69bdee0619
Akash Gaur <122721474+Gaur2526@users.noreply.github.com>
2023-12-10 11:05:58 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score = "33abc"
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let typeInNumber = Number(score)
-console.log(typeof typeInNumber);
-console.log(typeInNumber)
+// console.log(typeof typeInNumber);
+// console.log(typeInNumber)
 
 // "33"=> 33
 // "33abc" =>nan
@@ -13,9 +13,38 @@ console.log(typeInNumber)
 let isLoggedIn ;
 
 let booleanisLoggedIn = Boolean(isLoggedIn);
-console.log(booleanisLoggedIn)
+// console.log(booleanisLoggedIn)
 
 let number  = 33
 
 let stringnumber = String(number)
-console.log(typeof stringnumber)
+// console.log(typeof stringnumber)
+
+// ****************Operations****************
+
+let value = 2;
+let negValue = -value;
+
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3) /* 2 raise to power 2*/
+// console.log(2/2)
+// console.log(2%3)
+
+let str1 = "akash";
+let str2 = " gaur";
+
+let str3 = str1+str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 3);
+// console.log(1 + 2 + "3");
+
+// console.log(+true)  /* convert true to 1*/
+// console.log(+"")
+
