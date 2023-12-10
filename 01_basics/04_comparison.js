db6279c7095c69e8e2d5981371bd8e20da4f2025
Akash Gaur <122721474+Gaur2526@users.noreply.github.com>
2023-12-10 11:24:39 +0000

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2<=1)
+// console.log(2==1)
+// console.log(2!=1)
+
+// console.log("2" > 1) /*treating string as number returns true*/
+// console.log(1 < "2")
+
+/*javascript treat equality check ===  and comparisons > < <= >= differently
+
+comparison treat null to a number 0 that's why 3rd console statement is true 0>=0 and first statement is false 0>0*/
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >=0)
+
+//  === known as strict check which also checks value with datatype
+
+console.log("2" === 2) /* return false*/
+console.log("2" == 2) /* returns false*/
