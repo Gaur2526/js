e8dd65dd3d1fe3c6f303fc69f72efb71bc15599f
Akash Gaur <122721474+Gaur2526@users.noreply.github.com>
2023-12-12 09:34:00 +0000

    dates

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,34 @@
+// dates
+
+/* months in js starts from 0 means jan*/
+/* time is in milliseconds*/
+
+const myDate = new Date();
+
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+
+// const myCreatedDate = new Date(2003,6,24);
+// console.log(myCreatedDate.toDateString());
+// const newCreatedDate = new Date(2003,11,24);
+// console.log(newCreatedDate.toDateString());
+
+const newDate = new Date("04-27-2021");
+// console.log(newDate.toLocaleString())
+
+let myTimeStamp =Date.now()
+
+// console.log(myTimeStamp)
+// console.log(newDate.getTime())
+
+/* conversion to seconds*/
+
+// console.log(Math.floor(Date.now()/1000));
+
+const new_Date = new Date();
+
+console.log(new_Date.getDay())
+console.log(new_Date.getMonth())
+console.log(new_Date.getDate())
