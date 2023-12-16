39ac00db75fc1edbcfd1f7058009780ae21eb147
Akash Gaur <122721474+Gaur2526@users.noreply.github.com>
2023-12-16 18:27:12 +0000

    arrow

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,31 @@
+/*this keyword refers the current context(value) */
+
+const user = {
+    username: "Akash",
+    price : 999,
+
+    welcomeMessage : function(){
+        console.log(`${this.username}, welcome to the website`)
+        // console.log(this)
+    }
+    
+}
+// user.welcomeMessage()
+// user.username="dholu"
+// user.welcomeMessage()
+// console.log(this) /*an empty object*/
+
+function chaii(){
+    const username = "Akash"
+    // console.log(this.username) /* undefined bcz this. is not valid in functions it is used in objects*/
+}
+chaii()
+
+/* *********************arrow function********************** */
+
+// const addTwoNum = (num1, num2) =>{ /*if we are using curly braces then we have to use return keyword */
+//       return num1+num2
+// }
+
+const addTwoNum = (num1,num2) => (num1+num2) /* implicit method : doesn't require return keyword*/
+console.log(addTwoNum(3,4))
