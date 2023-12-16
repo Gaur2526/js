28a89a14f0796efc96d60ef59df2b800d8c76593
Akash Gaur <122721474+Gaur2526@users.noreply.github.com>
2023-12-16 18:01:44 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -10,6 +10,50 @@ if(true){
     // const b = 100;
     c = 30; /* re declaration of variable c bcz of var*/
 }
-console.log(a); /* this will print that a is not declared*/
-console.log(b); /* this will print that b is not declared*/
-console.log(c); /*this will print the value of c bcz of var keyword */
+// console.log(a); /* this will print that a is not declared*/
+// console.log(b); /* this will print that b is not declared*/
+// console.log(c); /*this will print the value of c bcz of var keyword */
+
+/* *************************************************** */
+
+function one(){
+    const username = "Akash"
+
+    function two(){
+        const email ="akash@google.com"
+        // console.log(username);
+    }
+
+    // console.log(email); /* here two is considered as local scope and one is considered as global scope for the two so we cannot fetch value from the local scope to the global scope*/
+
+    two()
+}
+
+one()
+
+if(true){
+    const username="Akash"
+
+    if(username){
+        const website = " Youtube"
+        // console.log(username + website)
+
+    }
+    // console.log(website) /* cannot be accesed bcz of scope*/
+}
+// console.log(username) /* cannot be accesed bcz of scope*/
+
+/* *************** Hositing ****************************** */
+/* JavaScript Hoisting refers to the process whereby the interpreter appears to move the declaration of functions, variables, classes, or imports to the top of their scope, prior to execution of the code.
+*/
+
+addOne(5)
+function addOne(num){
+     return num + 1
+}
+
+
+// addTwo(6) /* this is hoisting where we call the variable before the initialization*/
+const addTwo = function(num){
+    return num + 2;
+}
