3339a95c34543bbce5cc8631bc42a7898833f8d9
Akash Gaur <122721474+Gaur2526@users.noreply.github.com>
2023-12-20 05:47:02 +0000

    while & do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,24 @@
+/*WHILE LOOP */
+
+let index = 0;
+
+while(index<=10){
+    //console.log(`value of index is : ${index}`);
+    index = index + 2;
+}
+
+const myArray = ['akash','nikanshu','tushar']
+let start = 0;
+
+while(start < myArray.length){
+    //console.log(`value is : ${myArray[start]}`)
+    start = start + 1;
+}
+
+/* DO WHILE LOOP */
+
+let score = 11;
+do{
+     console.log(`score is : ${score}`)
+     score++
+}while(score<=10)
