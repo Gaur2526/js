d50923ba8da3e00f7573a7d5e38fb49ef01aed14
Akash Gaur <122721474+Gaur2526@users.noreply.github.com>
2024-02-20 18:48:40 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,10 +1,12 @@
 const accountId = 1325
+// comst means once value is assigned can't be changed
 let accountEmail = "akash@google.com"
 var accountPassword = "hgdhgdj"
-accountCity= "noida"
-let accountState;
+accountCity= "noida"  /*js allow us to assign a memory location to a variable without declaration of it's type but don't do this bcz of not a good practice */
 
-// accountId= 2;
+let accountState;
+ 
+// accountId= 2; /* cannot be assigned bcz of const variable  */
 
 accountEmail = "gaur@gmail.com"
 accountPassword="7454"
@@ -12,6 +14,9 @@ accountCity = "gr. noida"
 console.log(accountId);
 
 console.table([accountId, accountEmail, accountPassword,accountCity,accountState])
+
+/* table method helps to skip multiple console statements and represent the data in tabular form 
+syntax : console.table([variable_1,...,variable_n]) */
 
 /*
 Prefer not to use var
