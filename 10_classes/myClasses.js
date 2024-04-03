f73f529d08b8155d911cab403b5155084e356039
Akash Gaur <122721474+Gaur2526@users.noreply.github.com>
2024-04-03 16:18:00 +0000

    classes

---
+++ 10_classes/myClasses.js
@@ -0,0 +1,28 @@
+/* new keyword call automatically the constructor */
+
+class User{
+
+      constructor(username,email,password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+      }
+
+      encryptPassword(){
+        return `${this.password}abc`;
+      }
+
+      changeusername(){
+
+        return `${this.username.toUpperCase()}`;
+      }
+
+}
+
+const chai = new User('chai','chai@google.com','123');
+console.log(chai.encryptPassword());
+console.log(chai.changeusername());
+
+/*Behind the scene*/
+
+function User()
