34ea086b783223676909cba9a4ca5931213f2496
Akash Gaur <122721474+Gaur2526@users.noreply.github.com>
2024-04-08 18:15:28 +0000

    classes

--- 10_classes/myClasses.js
+++ 10_classes/myClasses.js
@@ -25,3 +25,20 @@ console.log(chai.changeusername());
 
 /*Behind the scene*/
 
+// function User(username,email,password){
+//         this.username = username;
+//         this.email = email;
+//         this.password = password;
+// }
+
+// User.prototype.encryptPassword = function(){
+//          return `${this.password}abc`;
+// }
+
+// User.prototype.changeusername = function(){
+//         return `${this.username.toUpperCase()}`;
+// }
+
+// const tea = new User('tea','tea@google.com','456');
+// console.log(tea.encryptPassword());
+// console.log(tea.changeusername());
